052487a18b9b3a73e1a8afec08efb3e3a7a32e93
Pablo Costanzo <costanzopa@gmail.com>
2021-05-24 16:25:52 -0300

    Adding Global styles

--- src/components/App/App.component.jsx
+++ src/components/App/App.component.jsx
@@ -8,7 +8,7 @@ const App = () => {
         <Jumbotron key={item.id} direction={item.direction}>
           <Jumbotron.Pane>
             <Jumbotron.Title>{item.title}</Jumbotron.Title>
-            <Jumbotron.Subtitle>{item.subtitle}</Jumbotron.Subtitle>
+            <Jumbotron.Subtitle>{item.subTitle}</Jumbotron.Subtitle>
           </Jumbotron.Pane>
           <Jumbotron.Pane>
             <Jumbotron.Image src={item.image} alt={item.alt} />
