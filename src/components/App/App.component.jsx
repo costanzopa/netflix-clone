8782b7a7458e849fffcd7cda54caba61b51bb4d9
Pablo Costanzo <costanzopa@gmail.com>
2021-05-27 18:09:10 -0300

    Adding constants routes

--- src/components/App/App.component.jsx
+++ src/components/App/App.component.jsx
@@ -1,7 +1,21 @@
-import HomePage from '../../pages/Home';
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import * as ROUTES from '../../constants/Routes';
+
+const HomePage = lazy(() => import('../../pages/Home'));
+const NotFoundPage = lazy(() => import('../../pages/NotFound'));
 
 const App = () => {
-  return <HomePage />;
+  return (
+    <Router>
+      <Suspense fallback={() => <div>Loading...</div>}>
+        <Switch>
+          <Route path={ROUTES.HOME} exact component={HomePage} />
+          <Route component={NotFoundPage} />
+        </Switch>
+      </Suspense>
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/pages/NotFound/NotFound.page.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const NotFoundPage = (props) => {
+  return <div>Lost Your Way</div>;
+};
+export default NotFoundPage;
