ea8497b3e67fd4a3278349d4218fc3c7905055cf
Pablo Costanzo <costanzopa@gmail.com>
2021-06-02 17:34:38 -0300

    Header Profile

--- src/components/Header/Header.component.jsx
+++ src/components/Header/Header.component.jsx
@@ -7,10 +7,42 @@ import {
   Logo,
   RightSideGroup,
   ButtonLink,
+  Feature,
+  Text,
+  FeatureCallOut,
+  Group,
+  Link,
+  Picture,
+  Dropdown,
+  Profile,
 } from './Header.style';
 
 const Header = ({ bg = true, children, ...restProps }) => {
   return bg ? <Background {...restProps}>{children}</Background> : children;
+};
+
+Header.Group = ({ children, ...restProps }) => {
+  return <Group {...restProps}>{children}</Group>;
+};
+
+Header.Logo = ({ to, ...restProps }) => {
+  return (
+    <ReactRouterLink to={to}>
+      <Logo {...restProps} />
+    </ReactRouterLink>
+  );
+};
+
+Header.Feature = ({ children, ...restProps }) => {
+  return <Feature {...restProps}>{children}</Feature>;
+};
+
+Header.Profile = ({ children, ...restProps }) => {
+  return <Profile {...restProps}>{children}</Profile>;
+};
+
+Header.Text = ({ children, ...restProps }) => {
+  return <Text {...restProps}>{children}</Text>;
 };
 
 Header.Frame = ({ children, ...restProps }) => {
@@ -31,6 +63,22 @@ Header.RightSideGroup = ({ children, ...restProps }) => {
 
 Header.ButtonLink = ({ children, ...restProps }) => {
   return <ButtonLink {...restProps}>{children}</ButtonLink>;
+};
+
+Header.FeatureCallOut = ({ children, ...restProps }) => {
+  return <FeatureCallOut {...restProps}>{children}</FeatureCallOut>;
+};
+
+Header.TextLink = ({ children, ...restProps }) => {
+  return <Link {...restProps}>{children}</Link>;
+};
+
+Header.Picture = ({ src, ...restProps }) => {
+  return <Picture {...restProps} src={`/images/users/${src}.png`} />;
+};
+
+Header.Dropdown = ({ children, ...restProps }) => {
+  return <Dropdown {...restProps}>{children}</Dropdown>;
 };
 
 export default Header;
