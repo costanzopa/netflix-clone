0af1075557b5b4f80f0be9686ca20369d4e95907
Pablo Costanzo <costanzopa@gmail.com>
2021-06-03 00:56:46 -0300

    Adding Card Component for Browse page

---
+++ src/components/Card/Card.component.jsx
@@ -0,0 +1,26 @@
+import React, { useState, createContext } from 'react';
+
+import { Container, Group, Title } from './Card.style';
+
+export const FeatureContext = createContext();
+
+export default function Card({ children, ...restProps }) {
+  const [showFeature, setShowFeature] = useState(false);
+  const [itemFeature, setItemFeature] = useState({});
+
+  return (
+    <FeatureContext.Provider
+      value={{ showFeature, setShowFeature, itemFeature, setItemFeature }}
+    >
+      <Container {...restProps}>{children}</Container>
+    </FeatureContext.Provider>
+  );
+}
+
+Card.Group = function CardGroup({ children, ...restProps }) {
+  return <Group {...restProps}>{children}</Group>;
+};
+
+Card.Title = function CardTitle({ children, ...restProps }) {
+  return <Title {...restProps}>{children}</Title>;
+};
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -10,3 +10,4 @@ export { default as Form } from './Form';
 export { default as Loader } from './Loader';
 export { default as Profiles } from './Profiles';
 export { default as Loading } from './Loading';
+export { default as Card } from './Card';
