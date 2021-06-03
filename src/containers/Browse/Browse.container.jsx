c7475c94f8d8447ae8912f00963fa934aa2555fd
Pablo Costanzo <costanzopa@gmail.com>
2021-06-03 17:23:36 -0300

    Adding Feature Cards styling

--- src/containers/Browse/Browse.container.jsx
+++ src/containers/Browse/Browse.container.jsx
@@ -84,7 +84,7 @@ const BrowseContainer = ({ slides }) => {
           <Card key={`${category}-${slideItem.title.toLowerCase()}`}>
             <Card.Title>{slideItem.title}</Card.Title>
             <Card.Entities>
-              {slideItem.map((item) => (
+              {slideItem.data.map((item) => (
                 <Card.Item key={item.docId} item={item}>
                   <Card.Image
                     src={`/images/${category}/${item.genre}/${item.slug}/small.jpg`}
@@ -96,6 +96,7 @@ const BrowseContainer = ({ slides }) => {
                 </Card.Item>
               ))}
             </Card.Entities>
+            <Card.Feature category={category}></Card.Feature>
           </Card>
         ))}
       </Card.Group>
