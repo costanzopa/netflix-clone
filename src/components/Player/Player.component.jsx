3a44658512f976d6aff239bde8642d4311a45bc7
Pablo Costanzo <costanzopa@gmail.com>
2021-06-04 23:38:21 -0300

    Adding Player component style

--- src/components/Player/Player.component.jsx
+++ src/components/Player/Player.component.jsx
@@ -1,6 +1,44 @@
-import React from 'react';
+import React, { useState, useContext, createContext } from 'react';
+import ReactDOM from 'react-dom';
 
-const Player = (props) => {
-  return <div>Player</div>;
+import { Container, Button, Overlay, Inner, Close } from './Player.style';
+
+export const PlayerContext = createContext();
+
+export default function Player({ children, ...restProps }) {
+  const [showPlayer, setShowPlayer] = useState(false);
+
+  return (
+    <PlayerContext.Provider value={{ showPlayer, setShowPlayer }}>
+      <Container {...restProps}>{children}</Container>
+    </PlayerContext.Provider>
+  );
+}
+
+Player.Video = function PlayerVideo({ src, ...restProps }) {
+  const { showPlayer, setShowPlayer } = useContext(PlayerContext);
+
+  return showPlayer
+    ? ReactDOM.createPortal(
+        <Overlay onClick={() => setShowPlayer(false)}>
+          <Inner>
+            <video id="netflix-player" controls>
+              <source src={src} type="video/mp4" />
+            </video>
+            <Close />
+          </Inner>
+        </Overlay>,
+        document.body
+      )
+    : null;
 };
-export default Player;
+
+Player.Button = function PlayerButton({ ...restProps }) {
+  const { showPlayer, setShowPlayer } = useContext(PlayerContext);
+
+  return (
+    <Button onClick={() => setShowPlayer(() => !showPlayer)} {...restProps}>
+      Play
+    </Button>
+  );
+};
