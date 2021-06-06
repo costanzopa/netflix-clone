21b028daeb33b422907cdb52c72ea27a8723fd57
Pablo Costanzo <costanzopa@gmail.com>
2021-06-06 19:30:25 -0300

    Adding Form Tests

--- src/components/Form/Form.component.jsx
+++ src/components/Form/Form.component.jsx
@@ -11,40 +11,38 @@ import {
   Submit,
 } from './Form.style';
 
-const Form = ({ children, ...restProps }) => {
+export default function Form({ children, ...restProps }) {
   return <Container {...restProps}>{children}</Container>;
-};
+}
 
-Form.Base = ({ children, ...restProps }) => {
-  return <Base {...restProps}>{children}</Base>;
-};
-
-Form.Error = ({ children, ...restProps }) => {
+Form.Error = function FormError({ children, ...restProps }) {
   return <Error {...restProps}>{children}</Error>;
 };
 
-Form.Title = ({ children, ...restProps }) => {
+Form.Base = function FormBase({ children, ...restProps }) {
+  return <Base {...restProps}>{children}</Base>;
+};
+
+Form.Title = function FormTitle({ children, ...restProps }) {
   return <Title {...restProps}>{children}</Title>;
 };
 
-Form.Text = ({ children, ...restProps }) => {
+Form.Text = function FormText({ children, ...restProps }) {
   return <Text {...restProps}>{children}</Text>;
 };
 
-Form.TextSmall = ({ children, ...restProps }) => {
+Form.TextSmall = function FormTextSmall({ children, ...restProps }) {
   return <TextSmall {...restProps}>{children}</TextSmall>;
 };
 
-Form.Link = ({ children, ...restProps }) => {
+Form.Link = function FormLink({ children, ...restProps }) {
   return <Link {...restProps}>{children}</Link>;
 };
 
-Form.Input = ({ children, ...restProps }) => {
+Form.Input = function FormInput({ children, ...restProps }) {
   return <Input {...restProps}>{children}</Input>;
 };
 
-Form.Submit = ({ children, ...restProps }) => {
+Form.Submit = function FormSubmit({ children, ...restProps }) {
   return <Submit {...restProps}>{children}</Submit>;
 };
-
-export default Form;
