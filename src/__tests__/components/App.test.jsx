eeac3b41bb21ab60fb1fa6554461183b8de65f7c
Pablo Costanzo <costanzopa@gmail.com>
2021-06-07 19:30:36 -0300

    Testing Browser Page test

--- src/__tests__/components/App.test.jsx
+++ src/__tests__/components/App.test.jsx
@@ -2,13 +2,30 @@ import React from 'react';
 import { fireEvent, render } from '@testing-library/react';
 import { App } from '../../components';
 import { FirebaseContext } from '../../context/firebase';
-import { firebase } from '../../lib/firabase.prod';
 import { useAuthListener } from '../../hooks';
 
 jest.mock('../../hooks');
 
+const mockFirebase = {
+  auth: jest.fn(() => ({
+    currentUser: {
+      displayName: 'Karl',
+      photoURL: 1,
+      email: 'karlhadwen@gmail.com',
+    },
+    signOut: jest.fn(() => Promise.resolve('I am signed out!')),
+  })),
+  firestore: jest.fn(() => ({
+    collection: jest.fn(() => ({
+      get: jest.fn(() => Promise.resolve('I get content!')),
+      add: jest.fn(() => Promise.resolve('I add content!')),
+    })),
+  })),
+};
+
 describe('<App />', () => {
   it('renders the <App /> Home Page', () => {
+    const firebase = mockFirebase;
     const { container } = render(
       <FirebaseContext.Provider value={{ firebase }}>
         <App />
@@ -18,6 +35,7 @@ describe('<App />', () => {
   });
 
   it('renders the <App /> Sign in Page', () => {
+    const firebase = mockFirebase;
     const { container, getByText } = render(
       <FirebaseContext.Provider value={{ firebase }}>
         <App />
@@ -30,6 +48,7 @@ describe('<App />', () => {
   });
 
   it('renders the <App /> Sign Up Page', async () => {
+    const firebase = mockFirebase;
     const { container, getByText } = render(
       <FirebaseContext.Provider value={{ firebase }}>
         <App />
@@ -40,6 +59,7 @@ describe('<App />', () => {
   });
 
   it('renders the <App /> Browse Page', async () => {
+    const firebase = mockFirebase;
     useAuthListener.mockReturnValue({});
     const { container } = render(
       <FirebaseContext.Provider value={{ firebase }}>
